bfb423277c888ef736d54e2edc37460045f72712
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 23:29:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   vals = name_hash.sort.collect{|key, value| key}
   vals2 = []
   vals.each do |pair|
-    vals2 << pair[0]
+    vals2 << pair
   end
   vals2.first
 end
