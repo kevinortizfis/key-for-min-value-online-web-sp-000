25b6e573db5d9574df6df4b3d1088aeb5700fd15
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 23:46:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  vals = name_hash.each do |a,b|
-    a[1] <=> b[1]
-  end
+  vals = name_hash.sort_by{|k,v| v}
   vals2 = []
   vals.each do |pair|
     vals2 << pair[0]
@@ -17,5 +15,5 @@ end
   #identify value in the hash
   #compare values to one another
   #set value-key pairs in order from smallest to largest
-#extract keys from new set of arrays
-#extract first element from new array
+#extract keys from new set of arrays - done, this is a nested array, so shovel array[element] works fine
+#extract first element from new array - done
