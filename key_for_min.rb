7a43ac6c7b463ae06fc3d6650070d6fb602b0dc3
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 23:49:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   vals = []
 
   def hash_sorter(name_hash)
-
+    name_hash.each do |k,v|
   end
 
   vals2 = []
@@ -19,7 +19,7 @@ end
 #to do:
 #sort the hash by value -- must not use sort/sort_by, etc.
   #identify value in the hash
-  #compare values to one another
+  #compare values to one another -- potentially compare arrays that are nested, flatten array first
   #set value-key pairs in order from smallest to largest
 #extract keys from new set of arrays - done, this is a nested array, so shovel array[element] works fine
 #extract first element from new array - done
