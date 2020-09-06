754e3be6cf401edf46ada9cbc756b7565ddb7c5d
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-06 23:47:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  vals = name_hash.sort_by{|k,v| v}
+  #vals = name_hash.sort_by{|k,v| v}
+  vals = []
   vals2 = []
   vals.each do |pair|
     vals2 << pair[0]
@@ -11,7 +12,7 @@ def key_for_min_value(name_hash)
 end
 
 #to do:
-#sort the hash by value:
+#sort the hash by value -- must not use sort/sort_by, etc.
   #identify value in the hash
   #compare values to one another
   #set value-key pairs in order from smallest to largest
